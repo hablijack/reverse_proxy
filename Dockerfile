b50001edd714c2cc62083468b7111845e2242dda
hablijack <christoph.habel@posteo.de>
2023-02-21 11:24:48 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM nginx:1.23-alpine
+
+# INSTALL CERTBOT
+RUN apk add certbot certbot-nginx
+
+# ADD DUMMY CERTS
+RUN mkdir -p /etc/letsencrypt/live/greenhouse.home-webserver.de
+COPY dummy_certs/* /etc/letsencrypt/live/greenhouse.home-webserver.de/
+
+# ADD ENTRYPOINT SCRIPT
+COPY entrypoint.sh /etc/nginx/entrypoint.sh
+RUN chmod a+x /etc/nginx/entrypoint.sh
+
+# NGINX CONFIG
+COPY app.conf /etc/nginx/conf.d/app.conf
+
+# CREATE WEBROOT FOR CERTBOT
+RUN mkdir -p /var/www/certbot
+
+CMD ["/etc/nginx/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# MANAGE LETSENCRYPT
+DIR="/etc/letsencrypt/archive"
+if [ -d "$DIR" ]; then
+  # RENEW CERTIFICATES
+  /usr/bin/certbot renew
+else
+  # START NGINX IN BACKGROUND
+  /usr/sbin/nginx
+
+  # INITIALIZE LETSENCRYPT
+  rm -rf /etc/letsencrypt/live
+  /usr/bin/certbot certonly -n --webroot -w /var/www/certbot --email christoph.habel@posteo.de -d greenhouse.home-webserver.de --rsa-key-size 4096 --agree-tos --force-renewal
+
+  # STOP BACKGROUND NGINX PROCESS
+  killall nginx
+fi
+
+# START NGINX IN FOREGROUND AND RUN FOREVER
+nginx -g 'daemon off;'
